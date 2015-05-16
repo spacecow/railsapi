003944dbeeaeb203f35348f788b30327ecbe5b9b
Johan Sveholm <jsveholm@gmail.com>
2015-05-17 04:11:27 +0900

    make protect_from_forgery accept json without error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery #with: :null_session,
+    #only: Proc.new { |c| c.request.format.json? }
 
   rescue_from ActiveRecord::StatementInvalid, with: :record_invalid
   rescue_from ActionController::ParameterMissing, with: :record_missing
