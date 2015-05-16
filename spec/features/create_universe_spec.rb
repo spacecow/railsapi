bbec355eebff13cf9bba08a6e3087e43cf2c9349
Johan Sveholm <jsveholm@gmail.com>
2015-05-16 19:18:14 +0900

    make features specs run less individual tests

--- spec/features/create_universe_spec.rb
+++ spec/features/create_universe_spec.rb
@@ -1,37 +1,32 @@
 require 'rails_helper'
 
-describe 'POST /universes' do
+describe 'Create universe' do
   let(:params){{ title:'Malazan' }}
   let(:driver){ Capybara.current_session.driver }
   let(:function){ driver.submit :post, universes_path, params }
   let(:body){ JSON.parse page.text }
 
-  context "success" do
-    it{ expect{function}.to change(
-      Universe,:count).by(1) }
-      
-    context "response" do
-      before{ function }
-      subject{ body }
-      it{ is_expected.to eq({
+  context "universe is valid" do
+    it "creates a universe" do
+      expect(Universe.count).to be 0
+      function
+      expect(Universe.count).to be 1
+      expect(body).to eq({
         "id" => Universe.first.id,
-        "title" => 'Malazan' }) }
+        "title" => 'Malazan' })
     end
-
-    after{ Universe.delete_all }
   end
-
-  context "failure" do
+  
+  context "universe is invalid" do
     let(:params){ {} }
-
-    it{ expect{function}.to_not change(
-      Universe,:count) }
-
-    context "response" do
-      before{ function }
-      subject{ body }
-      it{ is_expected.to have_key 'error' }
+    it "universe is not created" do
+      expect(Universe.count).to be 0
+      function
+      expect(Universe.count).to be 0
+      expect(body).to have_key 'error'
     end
   end
+
+  after{ Universe.delete_all }
 
 end
