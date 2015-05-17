732a9fd5c5ad4ecc6df64ddf6c28ab28c5efafdd
Johan Sveholm <jsveholm@gmail.com>
2015-05-18 01:05:05 +0900

    rails g doorkeeper:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require './lib/api_constraints'
 
 Rails.application.routes.draw do
+  use_doorkeeper
   namespace :api do
     scope module: :v1, constraints: ApiConstraints.new(version:1, default:true) do
       resources :universes, only: [:index, :create]
