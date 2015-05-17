a914257649c42c65bee9e98d8948a9430e0b0eed
Johan Sveholm <jsveholm@gmail.com>
2015-05-18 01:16:16 +0900

    temporarily create an admin with id 1
    browsing to: http://localhost:3000/oauth/authorize,
    generates "The authorization server does not support this response type."

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -6,7 +6,8 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
+    Struct.new(:id).new(1) 
+    # fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # Put your resource owner authentication logic here.
     # Example implementation:
     #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
