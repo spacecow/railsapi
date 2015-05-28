e7d3af9a548999e936a2531445fd39db894622da
Johan Sveholm <jsveholm@gmail.com>
2015-05-28 11:46:34 +0900

    do not be able to create a blank universe

--- db/migrate/20150516061839_create_universes.rb
+++ db/migrate/20150516061839_create_universes.rb
@@ -3,6 +3,7 @@ class CreateUniverses < ActiveRecord::Migration
     create_table :universes do |t|
       t.string :title, null:false
     end
+    execute "ALTER TABLE universes ADD CONSTRAINT title_cannot_be_blank CHECK (char_length(title) <> 0)"
     add_index :universes, :title, unique:true
   end
 
--- spec/models/universe_spec.rb
+++ spec/models/universe_spec.rb
@@ -8,8 +8,15 @@ describe Universe do
 
   it{ expect{model}.to change(Universe,:count).from(0).to(1) }
 
-  context "title is not set" do
+  context "title is nil" do
     let(:title){ nil }
+    it{ expect{model}.to raise_error{|e|
+      expect(e).to be_a ActiveRecord::StatementInvalid
+    }} 
+  end
+
+  context "title is blank" do
+    let(:title){ "" }
     it{ expect{model}.to raise_error{|e|
       expect(e).to be_a ActiveRecord::StatementInvalid
     }} 
