79eb074625d56c7aec34716aed5f2c507b9ca6a7
Johan Sveholm <jsveholm@gmail.com>
2015-12-03 01:33:02 +0100

    add comment to relation show call

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -3,12 +3,17 @@ class Relation < ActiveRecord::Base
   belongs_to :target, class_name:"Article"
   belongs_to :origin, class_name:"Article"
 
+  has_many :references, as: :referenceable
+
   def self.inverse_type s
     { "Owner"     => "Owns",
       "Husband"   => "Wife",
       "Counselor" => "Counsels"}[s]
   end
 
-  def full_json; as_json only:[:type] end
+  def full_json; as_json({
+    only:[:id,:type],
+    include:{ references:{ only:[:id, :comment] }} })
+  end
 
 end
--- spec/features/api/v1/show_relation_spec.rb
+++ spec/features/api/v1/show_relation_spec.rb
@@ -4,18 +4,24 @@ describe "Show relation" do
 
   let(:response){ JSON.parse(page.text)['relation'] }
   let(:relation){ create :relation, type:"Husband" }
+  let(:reference){ create :reference, referenceable:relation, comment:"yeah" }
 
   before do
+    reference
     visit api_relation_path relation
   end
 
   it "Exists" do
     expect(response).to eq({
-      'type'           => "Husband" 
-    })
+      'id'         => relation.id,
+      'type'       => "Husband",
+      'references' => [{
+        'id'         => reference.id,
+        'comment'    => "yeah" }] })
   end
 
   after do
+    Reference.delete_all
     Relation.delete_all
     Article.delete_all
     Universe.delete_all
