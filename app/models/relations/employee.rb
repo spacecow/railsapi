985e0ee2f685a62b5f008962d8aa21cadf597720
Johan Sveholm <jsveholm@gmail.com>
2015-12-28 04:13:56 +0100

    update categories

---
+++ app/models/relations/employee.rb
@@ -0,0 +1,2 @@
+class Employee < Relation
+end
--- spec/features/api/v1/list_relation_types_spec.rb
+++ spec/features/api/v1/list_relation_types_spec.rb
@@ -8,10 +8,10 @@ describe 'List relation types' do
 
   context 'response' do
     it{ expect(response).to eq(
-      %w(Acquaintance Advisor Aunt Betrothed Brother Counselor Courter Father
-         Follower Friend Guide Home Husband King Located_in Maid Member Mother
-         Near_sister Owner Player Practitioner Queen Resident Right_hand Sister
-         Swordbearer Teacher Uncle Variant Warder Wielder)) }
+      %w(Acquaintance Advisor Aunt Betrothed Brother Counselor Courter Employee
+         Father Follower Friend Guide Home Husband King Located_in Maid Member
+         Mother Near_sister Owner Player Practitioner Queen Resident Right_hand
+         Sister Swordbearer Teacher Uncle Variant Warder Wielder)) }
   end
 
 end
